0f7b116788b00f3edd1b19beeec71f2a828a6e95
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 04:48:10 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,7 +6,7 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
@@ -14,5 +14,5 @@ def second_challenge
       :heroine_friends => []
       }}
 
-  
+
 end
